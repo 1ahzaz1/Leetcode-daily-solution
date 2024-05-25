07a500a6f52b0f2aa1d52920cef4210019d1a18d
ahzaz <ahzaztahir448@gmail.com>
2024-05-25 02:24:21 +0100

    25/05/2024

---
+++ 1255. Maximum Score Words Formed by Letters.py
@@ -0,0 +1,43 @@
+class Solution:
+    def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
+        
+     
+        letter_count = Counter(letters) #Store letter as hashmap instead of list (Counter already imported from collections)
+        
+        #helper function called later:
+        def word_possible(word, letter_count):
+            word_count = Counter(word)
+            for char in word_count:
+                if word_count[char] > letter_count[char]:
+                    return False
+            return True
+
+
+        #helper function called later:
+        def get_score(word):
+            output = 0
+            for char in word:    
+                output += score[ord(char) - ord('a')] #Could do -97 instead of 'a', we are just converting to ASCII
+            return output
+        #Backtracking solution (4th day in a row).
+
+        def dfs(i): #Branch each combination of selecting words
+            if i == len(words): #On last word
+                return 0
+            #At every step we can either include the word or skip it
+
+            output = dfs(i+1) #Skip route
+
+            #Include route if possible:
+            if word_possible(words[i], letter_count):
+                #Create the word by decrementing letter count appropriately:
+                for char in words[i]:
+                    letter_count[char] -=1
+                output = max(output, get_score(words[i]) + dfs(i+1)) #Now we move on to next after decremnting from letters since this is included
+                for char in words[i]:
+                    letter_count[char] +=1
+                #Above we reversed the decrementing AFTER the recursive call, for proper use in other routes
+
+            return output
+
+        return dfs(0)
---
+++ 78. Subsets.py
@@ -0,0 +1,28 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+
+        #Backtracking solution:
+
+        #There are 2^n subsets
+        #We start from index 0
+        #At each point we branch out into a new possible subset that can be made
+        #This is done by having 2 options at each index;
+        #The subset INCLUDES nums[i] or it does NOT INCLUDE nums[i]
+        #At each index we have *2 possible routes. Until final index where we have 2^len(nums) routes
+
+        output = []
+        subset = []
+
+        def dfs(i):
+            if i >= len(nums):
+                output.append(subset.copy())
+                return
+            
+            subset.append(nums[i])
+            dfs(i+1)
+
+            subset.pop()
+            dfs(i+1)
+
+        dfs(0)
+        return output
