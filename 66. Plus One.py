c8ba35ab235a7af6ae9bba9d0c0251c706240fde
1ahzaz1 <ahzaztahir448@gmail.com>
2026-01-01 21:46:16 +0000

    01/01/2026

---
+++ 66. Plus One.py
@@ -0,0 +1,10 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        
+        for j in range(len(digits)-1, -1, -1):
+            if digits[j] != 9:
+                digits[j] += 1
+                return digits
+            else:
+                digits[j] = 0
+        return [1] + digits
